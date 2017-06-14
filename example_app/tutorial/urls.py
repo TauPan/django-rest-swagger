ae7cde3c5d042a80cd63e64e598a7992ae005cae
Friedrich Delgado <delgado@dfn-cert.de>
2017-06-14 16:37:29 +0200

    DWIM attempt to add a JSON Hyperschema renderer

--- example_app/tutorial/urls.py
+++ example_app/tutorial/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_swagger.views import get_swagger_view
+
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.schemas import SchemaGenerator
+from rest_framework.views import APIView
+from rest_framework_swagger import renderers
+from jsonhyperschema_codec import JSONHyperSchemaCodec
 
 from snippets import views
 
@@ -9,10 +15,35 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
-schema_view = get_swagger_view(title='Snippets API')
+
+class JSONHyperSchemaRenderer(renderers.BaseRenderer):
+
+    media_type = 'application/schema+json'
+    format = 'swagger'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        codec = JSONHyperSchemaCodec()
+        return codec.dump(data)
+
+
+class SwaggerSchemaView(APIView):
+    permission_classes = [AllowAny]
+    renderer_classes = [
+        renderers.OpenAPIRenderer,
+        renderers.SwaggerUIRenderer,
+        JSONHyperSchemaRenderer
+    ]
+
+    def get(self, request):
+        generator = SchemaGenerator()
+        schema = generator.get_schema(request=request)
+
+        return Response(schema)
+
 
 urlpatterns = [
-    url('^$', schema_view),
+    url('^$', SwaggerSchemaView.as_view()),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework'))
 ]
